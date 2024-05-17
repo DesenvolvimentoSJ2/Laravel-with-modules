e83e21275a581d8eebce70a031c5e63733b86c97
DesenvolvimentoSJ2 <135130880+DesenvolvimentoSJ2@users.noreply.github.com>
2024-05-17 17:27:20 -0300

    feat: add module Facs

---
+++ Modules/Facs/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('facs::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('facs.name') !!}
+    </p>
+@endsection
